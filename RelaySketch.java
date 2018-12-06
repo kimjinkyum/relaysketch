5c2fa28f3de991045c87576a57b13079a51164e1
parkchanbin <44570226+relaysketch@users.noreply.github.com>
2018-12-06 11:31:58 +0900

    Update RelaySketch.java

--- RelaySketch.java
+++ RelaySketch.java
@@ -1,46 +1,22 @@
 import java.awt.Dimension;
 
-
-
 import java.awt.EventQueue;
-
-
 
 import java.awt.Graphics;
 
-
-
 import java.awt.Image;
-
-
 
 import java.awt.Window;
 
-
-
-
-
-
-
 import javax.swing.ImageIcon;
-
-
 
 import javax.swing.JFrame;
 
-
-
 import javax.swing.JPanel;
-
-
 
 import javax.swing.JLabel;
 
-
-
 import java.awt.Font;
-
-
 
 import javax.swing.JTextField;
 
@@ -50,27 +26,13 @@ import javax.swing.border.EmptyBorder;
 
 import javax.swing.JButton;
 
-
-
 import javax.swing.JComboBox;
-
-
-
-
-
-
 
 import java.awt.event.ActionListener;
 
-
-
 import java.awt.event.ActionEvent;
 
-
-
 import java.awt.Color;
-
-
 
 import java.awt.BorderLayout;
 
@@ -78,148 +40,56 @@ import java.awt.BorderLayout;
 
 public class RelaySketch {
 
-
-public static int bfu=0;
-public static int afu=0;
-
-
-
-
    public static String IPADDRESS;
-
-
-
-   public static int check_IP=0;//∆–≈∞¡ˆ ∫Øºˆ
-
-
+   public static int afu=0;
+   public static int bfu=0;
+   public static int check_IP=0; //Ìå®ÌÇ§ÏßÄ Î≥ÄÏàò
 
    public static String NAME;
 
-
-
    public static int check_name=0;
-
-
 
    public static String TEAM;
 
-
-
    public static int check_team;
-
-
-
-
-
-
-
-   
-
-
-
-   
-
-
 
    private JFrame frame;
 
-
-
    private JTextField txtEnterIpAddress;
-
-
 
    private JTextField txtEnterYourName;
 
-
-
-   
-
-
-
-
-
-
-
    /**
-
-
 
     * Launch the application.
 
-
-
     */
-
-
 
    public static void main(String[] args) {
 
-
-
-      
-
-
-
       new RelaySketch();
-
-
 
    }
 
-
-
-
-
-
-
    /**
-
-
 
     * Create the application.
 
-
-
     */
-
-
 
    public RelaySketch() {
 
-
-
       initialize();
-
-
 
       frame.setVisible(true);
 
-
-
    }
-
-
-
-
-
-
 
    /**
 
-
-
     * Initialize the contents of the frame.
 
-
-
     */
-
-
-
-
-
-
 
    public void initialize() {
 
@@ -228,22 +98,41 @@ public static int afu=0;
       frame_ip.setSize(300,100);
 
       JFrame frame_name=new JFrame();
+
       JFrame frame_nameDU = new JFrame();
 
       frame_name.setSize(300,100);   
-      frame_nameDU.setSize(300,100);   
 
-      frame_ip.setResizable(false);//ªÁ¿Ã¡Ó ¡∂¡§x
+      frame_nameDU.setSize(300,100);
+
+      JFrame frame_A=new JFrame();
+
+      JFrame frame_B = new JFrame();
+
+      frame_A.setSize(600,100);   
+
+      frame_B.setSize(600,100);
+
+
+      frame_ip.setResizable(false);//ÏÇ¨Ïù¥Ï¶à Ï°∞Ï†ïx
 
       frame_ip.setLocationRelativeTo(null);
 
-      frame_name.setResizable(false);//ªÁ¿Ã¡Ó ¡∂¡§x
+      frame_name.setResizable(false);//ÏÇ¨Ïù¥Ï¶à Ï°∞Ï†ïx
 
       frame_name.setLocationRelativeTo(null);
 
-      frame_nameDU.setResizable(false);//ªÁ¿Ã¡Ó ¡∂¡§x
+      frame_nameDU.setResizable(false);//ÏÇ¨Ïù¥Ï¶à Ï°∞Ï†ïx
 
       frame_nameDU.setLocationRelativeTo(null);
+
+      frame_A.setResizable(false);//ÏÇ¨Ïù¥Ï¶à Ï°∞Ï†ïx
+
+      frame_A.setLocationRelativeTo(null);
+      frame_B.setResizable(false);//ÏÇ¨Ïù¥Ï¶à Ï°∞Ï†ïx
+
+      frame_B.setLocationRelativeTo(null);
+
 
       JPanel contentPane1;
 
@@ -253,21 +142,21 @@ public static int afu=0;
 
       contentPane1.setLayout(new BorderLayout(0, 0));
 
-      
 
-      JLabel lblIpChecked = new JLabel("ip checked!");
+
+      JLabel lblIpChecked = new JLabel("IP checked!");
 
       lblIpChecked.setBackground(new Color(102, 51, 51));
 
-      lblIpChecked.setForeground(new Color(255, 0, 0));
+      lblIpChecked.setForeground(Color.RED);
 
-      lblIpChecked.setFont(new Font("Arial Black", Font.PLAIN, 30));
+      lblIpChecked.setFont(new Font("Calibri", Font.BOLD, 30));
 
       lblIpChecked.setHorizontalAlignment(SwingConstants.CENTER);
 
-      
 
-   
+
+      
 
       contentPane1.add(lblIpChecked, BorderLayout.CENTER);
 
@@ -282,8 +171,9 @@ public static int afu=0;
       contentPane2.setBorder(new EmptyBorder(5, 5, 5, 5));
 
       contentPane2.setLayout(new BorderLayout(0, 0));
-      
-      
+
+
+
       JPanel contentPane2DU;
 
       contentPane2DU = new JPanel();
@@ -293,139 +183,122 @@ public static int afu=0;
       contentPane2DU.setLayout(new BorderLayout(0, 0));
 
       
-      
 
-      
-
-      JLabel lblIpChecked2 = new JLabel("name checked!");
+      JLabel lblIpChecked2 = new JLabel("Name checked!");
 
       lblIpChecked2.setBackground(new Color(102, 51, 51));
 
       lblIpChecked2.setForeground(new Color(255, 0, 0));
 
-      lblIpChecked2.setFont(new Font("Arial Black", Font.PLAIN, 30));
+      lblIpChecked2.setFont(new Font("Calibri", Font.BOLD, 30));
 
       lblIpChecked2.setHorizontalAlignment(SwingConstants.CENTER);
 
+
+     
       
-      
-      
-      JLabel lblIpChecked2DU = new JLabel("name is duplicated!");
+
+      JLabel lblIpChecked2DU = new JLabel("Name is duplicated!");
 
       lblIpChecked2DU.setBackground(new Color(102, 51, 51));
 
       lblIpChecked2DU.setForeground(new Color(255, 0, 0));
 
-      lblIpChecked2DU.setFont(new Font("Arial Black", Font.PLAIN, 15));
+      lblIpChecked2DU.setFont(new Font("Calibri", Font.BOLD, 30));
 
       lblIpChecked2DU.setHorizontalAlignment(SwingConstants.CENTER);
 
-      
-      
+      JLabel lblIpChecked4 = new JLabel("A team is full! You are team B");
 
-   
+      lblIpChecked4.setBackground(new Color(102, 51, 51));
+
+      lblIpChecked4.setForeground(new Color(255, 0, 0));
+
+      lblIpChecked4.setFont(new Font("Calibri", Font.BOLD, 30));
+
+      lblIpChecked4.setHorizontalAlignment(SwingConstants.CENTER);
+
+      JLabel lblIpChecked5 = new JLabel("B team is full! You are team A");
+
+      lblIpChecked5.setBackground(new Color(102, 51, 51));
+
+      lblIpChecked5.setForeground(new Color(255, 0, 0));
+
+      lblIpChecked5.setFont(new Font("Calibri", Font.BOLD, 30));
+
+      lblIpChecked5.setHorizontalAlignment(SwingConstants.CENTER);
+
 
       contentPane2.add(lblIpChecked2, BorderLayout.CENTER);
+
       contentPane2DU.add(lblIpChecked2DU, BorderLayout.CENTER);
+
       frame_name.add(contentPane2, BorderLayout.CENTER);
 
+      frame_nameDU.add(contentPane2DU, BorderLayout.CENTER);     
 
-      frame_nameDU.add(contentPane2DU, BorderLayout.CENTER);
-
-
-
-      
-
-      
+      frame_A.add(lblIpChecked4, BorderLayout.CENTER);
+      frame_B.add(lblIpChecked5, BorderLayout.CENTER);
 
       frame=new JFrame();
 
-
-
-      ImagePanel welcomePanel=new ImagePanel(new ImageIcon("C:\\2-2\\pencil.jpg").getImage());
-
-
+      ImagePanel welcomePanel=new ImagePanel(new ImageIcon("C:\\2-2\\beforegame.jpg").getImage());
 
       frame.setSize(1280,720);
-
-
 
       frame.getContentPane().add(welcomePanel, BorderLayout.EAST);
 
 
 
+      JLabel lblIpAddress = new JLabel("IP Address:");
 
+      lblIpAddress.setFont(new Font("Calibri", Font.BOLD, 22));
 
-      
-
-      JLabel lblIpAddress = new JLabel("IP Address");
-
-
-
-      lblIpAddress.setFont(new Font("Arial Black", Font.PLAIN, 22));
-
-
-
-      lblIpAddress.setBounds(12, 165, 137, 45);
-
-
+      lblIpAddress.setBounds(30, 165, 137, 45);
 
       welcomePanel.add(lblIpAddress);
 
 
 
-      
-
-
-
       txtEnterIpAddress = new JTextField();
-
-
 
       txtEnterIpAddress.setText("Enter IP Address");
 
-
-
       txtEnterIpAddress.setToolTipText("Enter IP Address");
 
-
-
-      txtEnterIpAddress.setBounds(161, 167, 200, 50);
+      txtEnterIpAddress.setBounds(161, 167, 200, 50); 
 
 
 
       welcomePanel.add(txtEnterIpAddress);
 
-
-
       txtEnterIpAddress.setColumns(10);
 
-
+      
 
       
+
       JButton btnNewButton = new JButton("A");
-
-
 
       btnNewButton.setFont(new Font("Agency FB", Font.BOLD, 52));
 
-      btnNewButton.setEnabled(false);//º“øµ
+      
+
+      btnNewButton.setEnabled(false);//ÏÜåÏòÅ
+
+      
+
+      btnNewButton.setBackground(Color.WHITE); //ÏùÄÏÑú
+
 
 
       btnNewButton.addActionListener(new ActionListener() {
 
-
          public void actionPerformed(ActionEvent arg0) 
-
-
 
          {
 
-
-
             TEAM="A";
-
-
 
             check_team=1;
 
@@ -438,58 +311,43 @@ public static int afu=0;
                e.printStackTrace();
 
             }
-
+for(int i=0; i<1000; i++)
+{
+	if(afu==1) {
+		 frame_A.setVisible(true);
+	}
+}
             frame.setVisible(false);
-
-
 
          }
 
-
-
       });
 
-
+      
 
       btnNewButton.setBounds(161, 302, 80, 65);
-
-
 
       welcomePanel.add(btnNewButton);
 
 
 
-   
-
-
-
       JButton btnB = new JButton("B");
-
-
 
       btnB.setFont(new Font("Agency FB", Font.BOLD, 52));
 
-
+      btnB.setBackground(Color.WHITE);//ÏùÄÏÑú
 
       btnB.setBounds(281, 302, 80, 65);
 
+      btnB.setEnabled(false);//ÏÜåÏòÅ
 
-      btnB.setEnabled(false);//º“øµ
       btnB.addActionListener(new ActionListener() {
-
-
 
          public void actionPerformed(ActionEvent arg0) 
 
-
-
          {
 
-
-
             TEAM="B";
-
-
 
             check_team=1;
 
@@ -502,201 +360,139 @@ public static int afu=0;
                e.printStackTrace();
 
             }
-
+            for(int i=0; i<1000; i++)
+            {
+            	if(bfu==1)
+            		 frame_B.setVisible(true);
+            		
+            }
             frame.setVisible(false);
-
-
-
-
 
          }
 
+         });
 
-
-      });
-
-
-
-      welcomePanel.add(btnB);
-
-
+      welcomePanel.add(btnB);    
 
       
-
 
       JButton submit_ip=new JButton("Submit");
 
+      submit_ip.setFont(new Font("Calibri",Font.BOLD,25));
 
+      submit_ip.setBounds(375, 168, 137, 50);
 
-      submit_ip.setFont(new Font("Arial Black",Font.PLAIN,22));
-
-
-      submit_ip.setBounds(375, 168, 137, 49);
-
-
-
-      
-
-
+      submit_ip.setBackground(Color.WHITE);
 
       welcomePanel.add(submit_ip);
 
-      JLabel lblName = new JLabel("Name");
 
 
+      JLabel lblName = new JLabel("Name:");
 
-      lblName.setFont(new Font("Arial Black", Font.PLAIN, 22));
+      lblName.setFont(new Font("Calibri", Font.BOLD, 22));
 
-
-
-      lblName.setBounds(12, 234, 137, 45);
-
-
+      lblName.setBounds(70, 234, 137, 45);
 
       welcomePanel.add(lblName);
 
 
 
-      
-
 
 
       txtEnterYourName = new JTextField();
 
-
-
       txtEnterYourName.setToolTipText("Enter your name");
-
-
 
       txtEnterYourName.setText("Enter your name");
 
-
-
       txtEnterYourName.setBounds(161, 238, 200, 50);
 
-
-
       welcomePanel.add(txtEnterYourName);
-
-
 
       txtEnterYourName.setColumns(10);
 
 
 
-      
-
-
-
       JButton submit_name = new JButton("Submit");
 
+      submit_name.setFont(new Font("Calibri",Font.BOLD,25));
 
-
-      
-
-
-
-      submit_name.setFont(new Font("Arial Black",Font.PLAIN,22));
-   
-         submit_name.setEnabled(false);
-
-      
- 
-
-
-      submit_ip.addActionListener(new ActionListener() 
+      submit_name.setEnabled(false);
 
 
 
-         {
+      submit_ip.addActionListener(new ActionListener(){
 
             @Override
-
-
 
             public void actionPerformed(ActionEvent e) 
 
             {
-                submit_name.setEnabled(true);
 
+               submit_name.setEnabled(true);
 
-       
                IPADDRESS= txtEnterIpAddress.getText();
-               check_IP=1;                   
-               
+
+               check_IP=1;
+
                frame_ip.setVisible(true);
 
-
-
             }
-
-
-
-
-
-
 
          });
 
-
-
-      
-
-
-
-      
-
-
-
-      
-
-
-
-      
-
-
-
-      
- 
+   
 
       submit_name.addActionListener(new ActionListener() {
+
          public void actionPerformed(ActionEvent arg0) 
+
          {
+
             NAME=txtEnterYourName.getText();
+
             check_name=1;
-         
-                try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+
             
-            if(Client.uniqueName==1) {
-            	 btnNewButton.setEnabled(true); 
-            	 btnB.setEnabled(true); 
-                 frame_name.setVisible(true);
 
-            }
-            else {
-            	
-            	//¡ﬂ∫πµ∆¥Ÿ¥¬ «¡∑π¿” ∂ÁøÏ±‚!
-            	frame_nameDU.setVisible(true);
-            }
+            try {
 
-         }         
+                Thread.sleep(100);
 
-      }
-      );
-  
+             } catch (InterruptedException e) {
 
-  
+                e.printStackTrace();
+
+             }
+
+             
+
+             if(Client.uniqueName==1) {
+
+                 btnNewButton.setEnabled(true); 
+
+                 btnB.setEnabled(true); 
+
+                  frame_name.setVisible(true);
 
 
+
+             }
+
+             else {//Ï§ëÎ≥µÎêêÎã§Îäî ÌîÑÎ†àÏûÑ ÎùÑÏö∞Í∏∞!
+
+                frame_nameDU.setVisible(true);
+
+             }
+
+          }         
+
+      });
+
+      submit_name.setBackground(Color.WHITE); //ÏùÄÏÑú
 
       submit_name.setBounds(375, 238, 137, 50);
-
-
 
       welcomePanel.add(submit_name);
 
@@ -704,109 +500,45 @@ public static int afu=0;
 
       
 
+      JLabel lblTeam = new JLabel("Team:");
 
+      lblTeam.setFont(new Font("Calibri", Font.BOLD, 22));
 
-      
-
-
-
-      
-
-
-
-      JLabel lblTeam = new JLabel("Team");
-
-
-
-      lblTeam.setFont(new Font("Arial Black", Font.PLAIN, 22));
-
-
-
-      lblTeam.setBounds(12, 305, 137, 50);
-
-
+      lblTeam.setBounds(70, 305, 137, 50);
 
       welcomePanel.add(lblTeam);
 
 
 
-      
-
-
-
-    
-
-
+      welcomePanel.add(btnB);
 
       JLabel lblRelaySketch = new JLabel("RELAY SKETCH");
 
+      lblRelaySketch.setForeground(Color.WHITE); //ÏùÄÏÑú
 
+      lblRelaySketch.setFont(new Font("Abadi", Font.BOLD| Font.ITALIC , 54));
 
-      lblRelaySketch.setForeground(Color.GRAY);
-
-
-
-      lblRelaySketch.setFont(new Font("Arial Rounded MT Bold", Font.BOLD | Font.ITALIC, 54));
-
-
-
-      lblRelaySketch.setBounds(407, 24, 462, 82);
-
-
+      lblRelaySketch.setBounds(400, 24, 470, 82);
 
       welcomePanel.add(lblRelaySketch);
 
 
 
-      
-
-
-
-   
-
-
-
-      
-
-
-
-      
-
-
-
-      frame.setResizable(false);//ªÁ¿Ã¡Ó ¡∂¡§x
-
-
+      frame.setResizable(false);//ÏÇ¨Ïù¥Ï¶à Ï°∞Ï†ïx
 
       frame.setLocationRelativeTo(null);
 
-
-
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-      
-
-   
-
-   
-
-
 
    }
 
+}      
 
 
-}
 
 
 
 class ImagePanel extends JPanel{
-
-
-
-
-
-
 
    private Image img;
 
@@ -814,23 +546,13 @@ class ImagePanel extends JPanel{
 
    public ImagePanel(Image img) {
 
-
-
       this.img=img;
-
-
 
       setSize(new Dimension(1280,720));
 
-
-
       setPreferredSize(new Dimension(1280,720));
 
-
-
       setLayout(null);
-
-
 
    }
 
@@ -838,11 +560,7 @@ class ImagePanel extends JPanel{
 
    public int getWidth() {
 
-
-
       return img.getWidth(null);
-
-
 
    }
 
@@ -850,30 +568,16 @@ class ImagePanel extends JPanel{
 
    public int getHeight() {
 
-
-
       return img.getHeight(null);
 
-
-
    }
-
-
 
    
 
-
-
    public void paintComponent(Graphics g) {
-
-
 
       g.drawImage(img, 0,0,null);
 
-
-
    }
-
-
 
 }
